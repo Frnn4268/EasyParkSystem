dad224528b2256d783e499cc511dc59abaaae2d3
Frnn4268 <appstutorialesy@gmail.com>
2024-04-07 23:50:04 -0600

    Adding new content for the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import "./App.css";
 
+// Home
+import Home from './pages/home/Home.jsx'; 
+import Information from './pages/home/Information.jsx'; 
+import Contact from './pages/home/Contact.jsx'; 
+
+// Auth
 import Register from './auth/Register';
 import Login from './auth/Login';
-import Dashboard from './pages/Dashboard';
-import Home from './pages/Home.jsx'; // Importa el componente Home
 import { useAuth } from './contexts/AuthContext.jsx';
+
+// Dashboard
+import Dashboard from './pages/Dashboard';
 
 const App = () => {
   const { isAuthenticated } = useAuth();
@@ -18,6 +25,18 @@ const App = () => {
           path="/"
           element={
             !isAuthenticated ? <Home /> : <Navigate to="/dashboard" />
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            !isAuthenticated ? <Information /> : <Navigate to="/about" />
+          }
+        />
+        <Route
+          path="/contact"
+          element={
+            !isAuthenticated ? <Contact /> : <Navigate to="/contact" />
           }
         />
         <Route
---
+++ frontend/src/pages/home/Information.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Descriptions, Layout } from 'antd';
+
+import MainMenu from './MainMenu'; 
+
+import '../../css/Home.css';
+
+const { Header } = Layout;
+
+const items = [
+    {
+        key: '1',
+        label: 'Nombre: ',
+        children: 'Restaurante y Pastelería Florencia',
+    },
+    {
+        key: '2',
+        label: 'Telefóno',
+        children: '7922 4557',
+    },
+    {
+        key: '3',
+        label: 'Horario: ',
+        children: 'Lunes a Domingo: 7:00 A.M. - 9:00 P.M.',
+    },
+    {
+        key: '4',
+        label: 'Departamento: ',
+        children: 'Jalapa, Jalapa',
+    },
+    {
+        key: '5',
+        label: 'Dirección',
+        children: 'Avenida Chipilapa 1-72, Jalapa 21001',
+    },
+  ];
+  
+const Information = () => {
+    return (
+        <>
+            <Layout className="layout">    
+                <Header className='home-header'> 
+                    <MainMenu />
+                </Header>
+                <Descriptions title="Información" items={items} />;
+            </Layout>   
+            
+        </>
+    )
+}
+
+export default Information;
