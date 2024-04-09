a0ca85565dded1a3c60bb41491528c32ed1613ba
Frnn4268 <appstutorialesy@gmail.com>
2024-04-09 00:01:52 -0600

    Uploading more features in the frontend

--- frontend/src/auth/Login.jsx
+++ frontend/src/auth/Login.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
-import { Card, Flex, Typography, Form, Input, Button, Alert, Spin, AutoComplete, FloatButton } from 'antd' 
+import { Card, Flex, Typography, Form, Input, Button, Alert, Spin, AutoComplete, FloatButton, Layout } from 'antd' 
 import { Link } from 'react-router-dom'
 
 import '../css/Auth.css'
-
+import form_card from '../assets/form_card2.png'
 import loginImage from '../assets/login_logo1.jpg'
+
 import useLogin from '../hooks/useLogin.jsx'
 
 import { HomeFilled, InfoCircleFilled } from '@ant-design/icons';
+
+const backgroundStyle = {
+  backgroundImage: `linear-gradient(to bottom, rgba(255, 255, 255, 0), rgba(255, 255, 255, 3)), url(${form_card})`,
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  position: 'relative',
+  borderRadius: '100% 0 0 50%',
+  width: '100%'
+};
 
 const Login = () => {
   const { loading, error, loginUser } = useLogin()
@@ -31,116 +41,119 @@ const Login = () => {
 
   return (
     <>
-      <div className='login-container'>
-        {/* Floating button Home and Information */}
-        <div className='float-button'>
-          <FloatButton.Group shape="circle" style={{ marginRight: 20 }} >
-            <Link to="/">
-              <FloatButton className='float-button-color' style={{marginBottom: 20}} icon={<HomeFilled />} />
-            </Link>
-            <Link to="/about">
-              <FloatButton className='float-button-color' icon={<InfoCircleFilled />} />
-            </Link>
-          </FloatButton.Group>
-        </div>
+      <Layout className="layout" style={backgroundStyle}>
+        <div className='login-container'>
+          {/* Floating button Home and Information */}
+          <div className='float-button'>
+            <FloatButton.Group shape="square" style={{ marginRight: 20 }} >
+              <Link to="/">
+                <FloatButton className='float-button-color' style={{marginBottom: 20}} icon={<HomeFilled />} />
+              </Link>
+              <Link to="/about">  
+                <FloatButton className='float-button-color' icon={<InfoCircleFilled />} >
+                </FloatButton>
+              </Link>
+            </FloatButton.Group>
+          </div>
 
-        {/* Content */}
-        <Card className='form-container'>
-          <Flex gap='large' align='center'>
-            {/* Image */}
-            <Flex flex={1}>
-              <img src={loginImage} className='auth-image'></img>
-            </Flex>
+          {/* Content */}
+          <Card className='form-container'>
+            <Flex gap='large' align='center'>
+              {/* Image */}
+              <Flex flex={1}>
+                <img src={loginImage} className='auth-image'></img>
+              </Flex>
 
-            {/* Form */}
-            <Flex vertical flex={1}>
-              <Typography.Title level={3} strong className='title'>
-                Iniciar sesión
-              </Typography.Title>
-              <Typography.Text type='secondary' strong className='slogan'>
-                EasyPark - Restaurante Florencia
-              </Typography.Text>
-              
-              <Form 
-                layout='vertical' 
-                onFinish={handleLogin} 
-                autoComplete='off'>
-
-                <Form.Item
-                  label='Email'
-                  name='email'
-                  rules={[
-                    {
-                      required: true,
-                      message: 'Por favor ingresa tu email',
-                    },
-                    {
-                      type: 'email',
-                      message: 'Este no es un email válido',
-                    },
-                  ]}
-                >
-                  <AutoComplete
-                    size='large'
-                    style={{ width: '100%' }}
-                    onSearch={handleSearch}
-                    placeholder="Ingresa tu email"
-                    options={options}
-                  />
-                </Form.Item>
-
-                  <Form.Item 
-                    label='Contraseña' 
-                    name='password' 
-                    rules={[{
-                      required: true,
-                      message: 'Por favor ingresa tu contraseña'
-                    }]}>
-                      <Input.Password size='large' placeholder='Ingresa tu contraseña'/>
-                  </Form.Item>
-
-                  {
-                    error && (
-                    <Alert 
-                      description={error} 
-                      type='error' 
-                      showIcon 
-                      closable 
-                      className='alert' />
-                    )
-                  }
-
-                  <Form.Item >
-                    <Button 
-                      type={`${loading ? '': 'primary'}`}
-                      htmlType='submit' 
-                      size='large' 
-                      className='btn'>
-                        { loading ? <Spin /> : 'Iniciar sesión' }
-                    </Button>
-                  </Form.Item>
-
-                  {/* Registro */}
-                  <Flex className="register-container">
-                    <Form.Item>
-                      <Typography.Text>
-                        ¿No tienes una cuenta?
-                      </Typography.Text>
-                    </Form.Item>
-                  </Flex>
+              {/* Form */}
+              <Flex vertical flex={1}>
+                <Typography.Title level={3} strong className='title'>
+                  Iniciar sesión
+                </Typography.Title>
+                <Typography.Text type='secondary' strong className='slogan'>
+                  EasyPark - Restaurante Florencia
+                </Typography.Text>
                 
-                  <Form.Item>
-                    <Link to='/register'>
-                      <Button size='large' className='btn'>
-                        Registrarse
-                      </Button>
-                    </Link>
+                <Form 
+                  layout='vertical' 
+                  onFinish={handleLogin} 
+                  autoComplete='off'>
+
+                  <Form.Item
+                    label='Email'
+                    name='email'
+                    rules={[
+                      {
+                        required: true,
+                        message: 'Por favor ingresa tu email',
+                      },
+                      {
+                        type: 'email',
+                        message: 'Este no es un email válido',
+                      },
+                    ]}
+                  >
+                    <AutoComplete
+                      size='large'
+                      style={{ width: '100%' }}
+                      onSearch={handleSearch}
+                      placeholder="Ingresa tu email"
+                      options={options}
+                    />
                   </Form.Item>
-              </Form>  
+
+                    <Form.Item 
+                      label='Contraseña' 
+                      name='password' 
+                      rules={[{
+                        required: true,
+                        message: 'Por favor ingresa tu contraseña'
+                      }]}>
+                        <Input.Password size='large' placeholder='Ingresa tu contraseña'/>
+                    </Form.Item>
+
+                    {
+                      error && (
+                      <Alert 
+                        description={error} 
+                        type='error' 
+                        showIcon 
+                        closable 
+                        className='alert' />
+                      )
+                    }
+
+                    <Form.Item >
+                      <Button 
+                        type={`${loading ? '': 'primary'}`}
+                        htmlType='submit' 
+                        size='large' 
+                        className='btn'>
+                          { loading ? <Spin /> : 'Iniciar sesión' }
+                      </Button>
+                    </Form.Item>
+
+                    {/* Registro */}
+                    <Flex className="register-container">
+                      <Form.Item>
+                        <Typography.Text>
+                          ¿No tienes una cuenta?
+                        </Typography.Text>
+                      </Form.Item>
+                    </Flex>
+                  
+                    <Form.Item>
+                      <Link to='/register'>
+                        <Button size='large' className='btn'>
+                          Registrarse
+                        </Button>
+                      </Link>
+                    </Form.Item>
+                </Form>  
+              </Flex>
             </Flex>
-          </Flex>
-        </Card>
-      </div>
+          </Card>
+        </div>
+      </Layout>
     </>
   ) 
 }
--- frontend/src/auth/Register.jsx
+++ frontend/src/auth/Register.jsx
@@ -1,13 +1,23 @@
-import { Card, Flex, Typography, Form, Input, Button, Alert, Spin, Select, AutoComplete, FloatButton } from 'antd' 
+import { Card, Flex, Typography, Form, Input, Button, Alert, Spin, Select, AutoComplete, FloatButton, Layout } from 'antd' 
 import React from 'react'
 import { Link } from 'react-router-dom'
 
 import '../css/Auth.css'
-
+import form_card from '../assets/form_card2.png'
 import registerImage from '../assets/register_logo1.jpg'
+
 import useSignup from '../hooks/useSingup'
 
 import { HomeFilled, InfoCircleFilled } from '@ant-design/icons';
+
+const backgroundStyle = {
+  backgroundImage: `linear-gradient(to bottom, rgba(255, 255, 255, 0), rgba(255, 255, 255, 3)), url(${form_card})`,
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  position: 'relative',
+  borderRadius: '100% 0 0 50%',
+  width: '100%'
+};
 
 const Register = () => {
   const { loading, error, registerUser } = useSignup()
@@ -31,146 +41,148 @@ const Register = () => {
 
   return (
     <>
-      <div className='register-container' >
-        {/* Floating button Home and Information */}
-        <div className='float-button'>
-          <FloatButton.Group shape="circle" style={{ marginRight: 20 }} >
-              <Link to="/">
-                <FloatButton style={{marginBottom: 20}} icon={<HomeFilled />} />
-              </Link>
-              <Link to="/about">
-                <FloatButton icon={<InfoCircleFilled />} />
-              </Link>
-          </FloatButton.Group>
-        </div>
+      <Layout style={backgroundStyle}>
+        <div className='register-container' >
+          {/* Floating button Home and Information */}
+          <div className='float-button'>
+            <FloatButton.Group shape="square" style={{ marginRight: 20 }} >
+                <Link to="/">
+                  <FloatButton style={{marginBottom: 20}} icon={<HomeFilled />} />
+                </Link>
+                <Link to="/about">
+                  <FloatButton icon={<InfoCircleFilled />} />
+                </Link>
+            </FloatButton.Group>
+          </div>
 
-        <Card className='form-container'>
-          <Flex gap='large' align='center'>
-            {/* Form */}
-            <Flex vertical flex={1}>
-              <Typography.Title level={3} strong className='title'>
-                Crear una cuenta
-              </Typography.Title>
-              <Typography.Text type='secondary' strong className='slogan'>
-                Crea una cuenta para ingresar a nuestra web!
-              </Typography.Text>
-              
-              <Form 
-                layout='vertical' 
-                onFinish={handleRegister} 
-                autoComplete='off'>
+          <Card className='form-container'>
+            <Flex gap='large' align='center'>
+              {/* Form */}
+              <Flex vertical flex={1}>
+                <Typography.Title level={3} strong className='title'>
+                  Crear una cuenta
+                </Typography.Title>
+                <Typography.Text type='secondary' strong className='slogan'>
+                  Crea una cuenta para ingresar a nuestra web!
+                </Typography.Text>
+                
+                <Form 
+                  layout='vertical' 
+                  onFinish={handleRegister} 
+                  autoComplete='off'>
 
-                  <Form.Item 
-                    label='Nombre completo' 
-                    name='name' 
-                    rules={[{
-                      required: true,
-                      message: 'Por favor ingresa tu nombre'
-                    }]}>
-                    <Input size='large' placeholder='Ingresa tu nombre completo'/>
-                  </Form.Item>
-
-                  <Form.Item 
-                    label='Email' 
-                    name='email' 
-                    rules={[{
-                      required: true,
-                      message: 'Por favor ingresa tu email'
-                    },
-                    {
-                      type: 'email',
-                      message: 'Este no es un email válido',
-                    },
-                    ]}
-                  >
-                    <AutoComplete
-                      size='large'
-                      style={{ width: '100%' }}
-                      onSearch={handleSearch}
-                      placeholder="Ingresa tu email"
-                      options={options}
-                    />
-                  </Form.Item>
-
-                  <Form.Item 
-                    label='Contraseña' 
-                    name='password' 
-                    rules={[{
-                      required: true,
-                      message: 'Por favor ingresa tu contraseña'
-                    }]}>
-                    <Input.Password size='large' placeholder='Ingresa tu contraseña'/>
-                  </Form.Item>
-
-                  <Form.Item 
-                    label='Confirmación de contraseña' 
-                    name='passwordConfirm' 
-                    rules={[{
-                      required: true,
-                      message: 'Por favor confirma tu contraseña'
-                    }]}>
-                    <Input.Password size='large' placeholder='Ingresa de nuevo tu contraseña'/>
-                  </Form.Item>
-
-                  <Form.Item 
-                    label='Rol'
-                    name='role'
-                    rules={[{
-                      required: true,
-                      message: 'Por favor selecciona tu rol'
-                    }]}>
-                    <Select size='large' placeholder='Selecciona tu rol'>
-                      <Select.Option value='Administrador'>Administrador</Select.Option>
-                      <Select.Option value='Empleado'>Empleado</Select.Option>
-                    </Select>
-                  </Form.Item>
-
-                  {
-                    error && (
-                    <Alert 
-                      description={error} 
-                      type='error' 
-                      showIcon 
-                      closable 
-                      className='alert' />
-                    )
-                  }
-
-                  <Form.Item >
-                    <Button 
-                      type={`${loading ? '': 'primary'}`}
-                      htmlType='submit' 
-                      size='large' 
-                      className='btn'>
-                        {loading ? <Spin /> : 'Crear cuenta'}
-                    </Button>
-                  </Form.Item>
-
-                  <Flex className="login-container">
-                    <Form.Item>
-                      <Typography.Text>
-                        ¿Ya tienes una cuenta?
-                      </Typography.Text>
+                    <Form.Item 
+                      label='Nombre completo' 
+                      name='name' 
+                      rules={[{
+                        required: true,
+                        message: 'Por favor ingresa tu nombre'
+                      }]}>
+                      <Input size='large' placeholder='Ingresa tu nombre completo'/>
                     </Form.Item>
-                  </Flex>
 
-                  <Form.Item>
-                    <Link to='/login'>
-                      <Button size='large' className='btn'>
-                        Iniciar sesión
+                    <Form.Item 
+                      label='Email' 
+                      name='email' 
+                      rules={[{
+                        required: true,
+                        message: 'Por favor ingresa tu email'
+                      },
+                      {
+                        type: 'email',
+                        message: 'Este no es un email válido',
+                      },
+                      ]}
+                    >
+                      <AutoComplete
+                        size='large'
+                        style={{ width: '100%' }}
+                        onSearch={handleSearch}
+                        placeholder="Ingresa tu email"
+                        options={options}
+                      />
+                    </Form.Item>
+
+                    <Form.Item 
+                      label='Contraseña' 
+                      name='password' 
+                      rules={[{
+                        required: true,
+                        message: 'Por favor ingresa tu contraseña'
+                      }]}>
+                      <Input.Password size='large' placeholder='Ingresa tu contraseña'/>
+                    </Form.Item>
+
+                    <Form.Item 
+                      label='Confirmación de contraseña' 
+                      name='passwordConfirm' 
+                      rules={[{
+                        required: true,
+                        message: 'Por favor confirma tu contraseña'
+                      }]}>
+                      <Input.Password size='large' placeholder='Ingresa de nuevo tu contraseña'/>
+                    </Form.Item>
+
+                    <Form.Item 
+                      label='Rol'
+                      name='role'
+                      rules={[{
+                        required: true,
+                        message: 'Por favor selecciona tu rol'
+                      }]}>
+                      <Select size='large' placeholder='Selecciona tu rol'>
+                        <Select.Option value='Administrador'>Administrador</Select.Option>
+                        <Select.Option value='Empleado'>Empleado</Select.Option>
+                      </Select>
+                    </Form.Item>
+
+                    {
+                      error && (
+                      <Alert 
+                        description={error} 
+                        type='error' 
+                        showIcon 
+                        closable 
+                        className='alert' />
+                      )
+                    }
+
+                    <Form.Item >
+                      <Button 
+                        type={`${loading ? '': 'primary'}`}
+                        htmlType='submit' 
+                        size='large' 
+                        className='btn'>
+                          {loading ? <Spin /> : 'Crear cuenta'}
                       </Button>
-                    </Link>
-                  </Form.Item>
-              </Form>  
-            </Flex>
+                    </Form.Item>
 
-            {/* Image */}
-            <Flex flex={1}>
-              <img src={registerImage} className='auth-image'></img>
+                    <Flex className="login-container">
+                      <Form.Item>
+                        <Typography.Text>
+                          ¿Ya tienes una cuenta?
+                        </Typography.Text>
+                      </Form.Item>
+                    </Flex>
+
+                    <Form.Item>
+                      <Link to='/login'>
+                        <Button size='large' className='btn'>
+                          Iniciar sesión
+                        </Button>
+                      </Link>
+                    </Form.Item>
+                </Form>  
+              </Flex>
+
+              {/* Image */}
+              <Flex flex={1}>
+                <img src={registerImage} className='auth-image'></img>
+              </Flex>
             </Flex>
-          </Flex>
-        </Card>
-      </div>
+          </Card>
+        </div>
+      </Layout>
     </>
   )
 }
