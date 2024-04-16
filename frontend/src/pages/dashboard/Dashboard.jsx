c3d158e1d63fe22348600eaaf37fea631d17ab12
Frnn4268 <appstutorialesy@gmail.com>
2024-04-16 17:43:05 -0600

    Adding new features

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Layout, Alert } from 'antd';
+import Marquee from 'react-fast-marquee';
 
 import TopMenu from './TopMenu.jsx';
 import LeftMenu from './LeftMenu.jsx';
@@ -14,9 +15,22 @@ const Dashboard = () => {
             <Header className='home-header-dashboard'>
                 <TopMenu />
             </Header>
-            <Content>
-                <LeftMenu />
-            </Content>
+            <Layout>
+                <Layout.Sider>
+                    <LeftMenu />
+                </Layout.Sider>
+                <Layout.Content>
+                    <Alert
+                            banner
+                            style={{ position: 'absolute', bottom: 20, left: '50%', transform: 'translateX(-50%)' }}
+                            message={
+                                <Marquee pauseOnHover gradient={false}>
+                                    Cantidad de espacios de parqueo disponibles: Cantidad de espacios de parqueo no disponibles:
+                                </Marquee>
+                            }
+                        />
+                </Layout.Content>
+            </Layout>
         </Layout>
     )
 }
