64c618158072dde49fef50a9c6b69565fa36681c
Frnn4268 <appstutorialesy@gmail.com>
2024-04-17 00:50:46 -0600

    Adding new features in the frontend

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Layout, Alert } from 'antd';
+import { Layout, Alert, Row, Statistic, Col, Card } from 'antd';
 import Marquee from 'react-fast-marquee';
 
 import TopMenu from './TopMenu.jsx';
 import LeftMenu from './LeftMenu.jsx';
 
-import '../../css/DashboardMenu.css'
+import '../../css/DashboardMenu.css';
+import logo_dashboard from '../../assets/home/logo_card.png';
 
-const { Header, Content } = Layout;
+const { Header } = Layout;
 
 const Dashboard = () => {
     return (
@@ -19,16 +20,45 @@ const Dashboard = () => {
                 <Layout.Sider>
                     <LeftMenu />
                 </Layout.Sider>
-                <Layout.Content>
+                <Layout.Content className='layout-content-dashboard'>
+                    <img src={logo_dashboard} className='logo-dashboard' />
                     <Alert
-                            banner
-                            style={{ position: 'absolute', bottom: 20, left: '50%', transform: 'translateX(-50%)' }}
-                            message={
-                                <Marquee pauseOnHover gradient={false}>
-                                    Cantidad de espacios de parqueo disponibles: Cantidad de espacios de parqueo no disponibles:
-                                </Marquee>
-                            }
-                        />
+                        banner
+                        className='alert-layout'
+                        message={
+                            <Marquee pauseOnHover gradient={false}>
+                                Cantidad de espacios de parqueo disponibles: Cantidad de espacios de parqueo no disponibles:
+                            </Marquee>
+                        }
+                    />
+                    <Row gutter={16}>
+                        <Col span={12}>
+                            <Card bordered={false}>
+                                <Statistic
+                                title="Active"
+                                value={11.28}
+                                precision={2}
+                                valueStyle={{
+                                    color: '#3f8600',
+                                }}
+                                suffix="%"
+                                />
+                            </Card>
+                        </Col>
+                        <Col span={12}>
+                            <Card bordered={false}>
+                                <Statistic
+                                title="Idle"
+                                value={9.3}
+                                precision={2}
+                                valueStyle={{
+                                    color: '#cf1322',
+                                }}
+                                suffix="%"
+                                />
+                            </Card>
+                        </Col>
+                    </Row>
                 </Layout.Content>
             </Layout>
         </Layout>
