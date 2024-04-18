02cbefb0c9064f63ecdf52ee39178cce31dee763
Frnn4268 <appstutorialesy@gmail.com>
2024-04-18 17:44:48 -0600

    Adding new Drawer to enter data and new disposition for Park.jsx

--- frontend/src/pages/park/Park.jsx
+++ frontend/src/pages/park/Park.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Layout, Button, ConfigProvider, Drawer, Space, Col, Form, Input, Row, Select } from 'antd';
+import { Layout, Button, ConfigProvider, Drawer, Space, Col, Form, Input, Row, Select, Card, Statistic } from 'antd';
 
 import TopMenu from '../dashboard/TopMenu.jsx';
 import LeftMenu from '../dashboard/LeftMenu.jsx';
@@ -10,8 +10,53 @@ import '../../css/Park.css';
 const { Header } = Layout;
 const { Option } = Select;
 
-const buttonWidth = 45;
-const buttonHeight = 90;
+const buttonWidth = 60;
+const buttonHeight = 120;
+
+const renderForm = () => {
+    return (
+        <Form layout="vertical" hideRequiredMark>
+            <Row gutter={16}>
+                <Col span={12}>
+                    <Form.Item
+                        name="name"
+                        label="Name"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please enter user name',
+                            },
+                        ]}
+                    >
+                        <Input placeholder="Please enter user name" />
+                    </Form.Item>
+                </Col>
+                <Col span={12}>
+                    <Form.Item
+                        name="url"
+                        label="Url"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please enter url',
+                            },
+                        ]}
+                    >
+                        <Input
+                            style={{
+                                width: '100%',
+                            }}
+                            addonBefore="http://"
+                            addonAfter=".com"
+                            placeholder="Please enter url"
+                        />
+                    </Form.Item>
+                </Col>
+            </Row>
+            {/* Resto del formulario... */}
+        </Form>
+    );
+};
 
 const Park = () => {
     const [openDrawer, setOpenDrawer] = useState(false);
@@ -67,7 +112,7 @@ const Park = () => {
                             style: {
                                 width: buttonWidth,
                                 height: buttonHeight,
-                                margin: 4,
+                                margin: 6,
                             },
                         }}
                     >
@@ -93,54 +138,69 @@ const Park = () => {
                         </div>
                     </ConfigProvider>
                     {renderDrawerContent()}
+                    <div className="center-right-container-parking">
+                        <Row gutter={20}>
+                            <Col span={40}>
+                                <Card bordered={false}>
+                                    <Statistic
+                                    title="Porcentaje de parqueo en uso"
+                                    value={100}
+                                    valueStyle={{
+                                        color: '#3f8600',
+                                    }}
+                                    suffix="%"
+                                    style={{ marginBottom: 20 }}
+                                    />
+                                    <Statistic
+                                    title="Porcentaje de parqueo en desuso"
+                                    value={0}
+                                    valueStyle={{
+                                        color: '#cf1322',
+                                    }}
+                                    style={{ marginBottom: 20 }}
+                                    suffix="%"
+                                    />
+                                    <Statistic
+                                    title="Espacios de parqueo libres"
+                                    value={19}
+                                    valueStyle={{
+                                        color: '#3f8600',
+                                    }}
+                                    style={{ marginBottom: 20 }}
+                                    />
+                                    <Statistic
+                                    title="Espacios de parqueo ocupados"
+                                    value={0}
+                                    valueStyle={{
+                                        color: '#cf1322',
+                                    }}
+                                    style={{ marginBottom: 20 }}
+                                    />
+                                    <Statistic
+                                    title="Porcentaje de parqueo en uso"
+                                    value={100}
+                                    valueStyle={{
+                                        color: '#3f8600',
+                                    }}
+                                    suffix="%"
+                                    style={{ marginBottom: 20 }}
+                                    />
+                                    <Statistic
+                                    title="Porcentaje de parqueo en uso"
+                                    value={100}
+                                    valueStyle={{
+                                        color: '#cf1322',
+                                    }}
+                                    suffix="%"
+                                    style={{ marginBottom: 20 }}
+                                    />
+                                </Card>
+                            </Col>
+                        </Row>
+                    </div>
                 </Layout.Content>
             </Layout>
         </Layout>
-    );
-};
-
-const renderForm = () => {
-    return (
-        <Form layout="vertical" hideRequiredMark>
-            <Row gutter={16}>
-                <Col span={12}>
-                    <Form.Item
-                        name="name"
-                        label="Name"
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Please enter user name',
-                            },
-                        ]}
-                    >
-                        <Input placeholder="Please enter user name" />
-                    </Form.Item>
-                </Col>
-                <Col span={12}>
-                    <Form.Item
-                        name="url"
-                        label="Url"
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Please enter url',
-                            },
-                        ]}
-                    >
-                        <Input
-                            style={{
-                                width: '100%',
-                            }}
-                            addonBefore="http://"
-                            addonAfter=".com"
-                            placeholder="Please enter url"
-                        />
-                    </Form.Item>
-                </Col>
-            </Row>
-            {/* Resto del formulario... */}
-        </Form>
     );
 };
 
