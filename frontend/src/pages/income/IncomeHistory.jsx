36b231a93faa11f2c7aa09af47d8759a9a069983
Frnn4268 <appstutorialesy@gmail.com>
2024-09-14 15:28:16 -0600

    Adding new validations in IncomeHistory.jsx

--- frontend/src/pages/income/IncomeHistory.jsx
+++ frontend/src/pages/income/IncomeHistory.jsx
@@ -189,25 +189,70 @@ const IncomeHistory = () => {
                 </Layout.Content>
             </Layout>
             <Drawer
-                title="Editar contacto"
+                title="Editar historial de ingresos"
                 width={500}
                 onClose={() => setDrawerVisible(false)}
                 visible={drawerVisible}
             >
                 <Form form={form} onFinish={onFinish}>
-                    <Form.Item label="Día" name="day">
+                    <Form.Item 
+                        label="Día" 
+                        name="day"
+                        rules={[
+                            {
+                                required: true,
+                                message: '¡Por favor ingresa el día del ingreso!',
+                            },
+                        ]}
+                    >
                         <Input />
                     </Form.Item>
-                    <Form.Item label="Mes" name="month">
+                    <Form.Item 
+                        label="Mes" 
+                        name="month"
+                        rules={[
+                            {
+                                required: true,
+                                message: '¡Por favor ingresa el mes del ingreso!',
+                            },
+                        ]}
+                    >
                         <Input />
                     </Form.Item>
-                    <Form.Item label="Año" name="year">
+                    <Form.Item 
+                        label="Año" 
+                        name="year"
+                        rules={[
+                            {
+                                required: true,
+                                message: '¡Por favor ingresa el año del ingreso!',
+                            },
+                        ]}
+                    >
                         <Input />
                     </Form.Item>
-                    <Form.Item label="Monto monetario" name="income">
+                    <Form.Item 
+                        label="Monto monetario" 
+                        name="income"
+                        rules={[
+                            {
+                                required: true,
+                                message: '¡Por favor ingresa el monto del ingreso!',
+                            },
+                        ]}
+                    >
                         <Input />
                     </Form.Item>
-                    <Form.Item label="Fecha y hora" name="hour_date">
+                    <Form.Item 
+                        label="Fecha y hora" 
+                        name="hour_date"
+                        rules={[
+                            {
+                                required: true,
+                                message: '¡Por favor ingresa la fecha y hora del ingreso!',
+                            },
+                        ]}
+                    >
                         <Input />
                     </Form.Item>
                     <Form.Item>
